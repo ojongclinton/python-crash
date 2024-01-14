11d2f21fdff1a3ed84be9d1b2b2be97ab2cab5e5
ojongclinton <sheyenoh123@gmail.com>
2024-01-14 13:21:57 +0100

    keyup and keydown events

--- project1/alien_invasion.py
+++ project1/alien_invasion.py
@@ -30,16 +30,24 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self.check_keydown_events(event)
 
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self.check_keyup_events(event)
+
+    def check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         #Update images on the screen and flip to the new screen.
--- project1/ship.py
+++ project1/ship.py
@@ -24,9 +24,10 @@ class Ship:
         self.moving_right = False
 
     def update(self):
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        elif self.moving_left:
+        elif self.moving_left and self.rect.left > 0:
+            print(self.rect.left)
             self.x -= self.settings.ship_speed
         self.rect.x = self.x
 
