c0d5590b5c6d5d5496069184d8544c5ed5ebb9c8
ojongclinton <sheyenoh123@gmail.com>
2024-01-14 11:38:54 +0100

    helper functions v1

--- project1/alien_invasion.py
+++ project1/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -13,17 +14,25 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
     def run_game(self):
         #Start the main loop for the game.
         while True:
-            #Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
+            self.clock.tick(60)
+            #Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             #Make the most recently drawn screen visible.
             pygame.display.flip()
+
+    def _check_events(self):
+        #Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
 
 if __name__ == "__main__":
