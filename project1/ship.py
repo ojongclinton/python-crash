0e41dec92a0653bf9c33212ab66777c26f17739f
ojongclinton <sheyenoh123@gmail.com>
2024-01-14 12:40:58 +0100

    movements to the left

--- project1/ship.py
+++ project1/ship.py
@@ -12,17 +12,23 @@ class Ship:
         #Load ship image and get its rect
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
-        self.moving_right = False
-        self.moving_left = False
+        self.settings = ai_game.settings
 
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
+
+        self.moving_left = False
+        self.moving_right = False
+
     def update(self):
         if self.moving_right:
-            self.rect.x += 2
+            self.x += self.settings.ship_speed
         elif self.moving_left:
-            self.rect.x -= 2
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         """Draw the ship at its current location"""
