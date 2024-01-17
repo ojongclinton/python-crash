9d38d0f930b8d2ca6c02db501d047957bbaf6b3d
ojongclinton <sheyenoh123@gmail.com>
2024-01-17 21:45:55 +0100

    print on init

--- project1/bullet.py
+++ project1/bullet.py
@@ -10,13 +10,15 @@ class Bullet(Sprite):
         self.settings = ai_game.settings
         self.color = ai_game.settings.bullet_color
 
-        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,self.settings.bullet_height)
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width,
+                                self.settings.bullet_height)
 
         self.rect.midtop = ai_game.ship.rect.midtop
         self.y = float(self.rect.y)
+        print(ai_game.ship.rect.midtop)
 
     def update(self):
-        self.y += self.settings.bullet_speed
+        self.y -= self.settings.bullet_speed
         self.rect.y = self.y
 
     def draw_bullet(self):
