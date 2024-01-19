005e18b40947f8ef4ad58dcc388d3cc96d1eb6de
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-19 21:55:40 +0100

    alien letf, right, up and down

--- project1/alien_invasion.py
+++ project1/alien_invasion.py
@@ -69,7 +69,19 @@ class AlienInvasion:
         print(len(self.bullets))
 
     def _update_aliens(self):
+        self._check_fleet_edges()
         self.aliens.update()
+
+    def _check_fleet_edges(self):
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._check_fleet_direction()
+                break
+    
+    def _check_fleet_direction(self):
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _check_events(self):
         #Watch for keyboard and mouse events.
