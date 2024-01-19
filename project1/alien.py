c0aa1435f28248f7a037463a5d3494c61b0af88d
ojongclinton <sheyenoh123@gmail.com>
2024-01-19 10:00:00 +0100

    added alien to game

---
+++ project1/alien.py
@@ -0,0 +1,19 @@
+from pygame.sprite import Sprite
+import pygame
+
+
+class Alien(Sprite):
+
+    def __init__(self, ai_game):
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        self.x = float(self.rect.x)
