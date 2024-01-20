5e4d85c23d8214c7ef836744e892517ff33c85c1
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-20 22:40:02 +0100

    remove collision code from update bullet code

--- project1/alien_invasion.py
+++ project1/alien_invasion.py
@@ -66,11 +66,15 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
-        collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+        self._check_bullet_alien_collisions()
         if not self.aliens:
             self.bullets.empty()
             self._create_fleet()
         # print(len(self.bullets))
+    
+    def _check_bullet_alien_collisions(self):
+        collisions = pygame.sprite.groupcollide(self.bullets,self.aliens,True,True)
+
 
 
     def _update_aliens(self):
