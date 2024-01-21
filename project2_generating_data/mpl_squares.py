fe8f1818f8a3f444ceb72d8dd0de071301d0d76d
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-21 07:24:46 +0100

    lince colors, gradiensts

--- project2_generating_data/mpl_squares.py
+++ project2_generating_data/mpl_squares.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 
-squares = [1, 4, 9, 16, 25]
+input_values = range(1,1001)
+squares = [x**2 for x in input_values]
 
+plt.style.use('seaborn')
 fig, ax = plt.subplots()
-ax.set_title("Square Numbers", fontsize=24)
-ax.set_xlabel("Value", fontsize=14)
-ax.set_ylabel("Square of Value", fontsize=14)
-# ax.tick_params(labelsize=14)
-ax.plot(squares,linewidth=1)
 
+ax.scatter(input_values, squares,c=squares ,cmap=plt.cm.Blues, s=1)
+
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)         
+ax.set_ylabel("Square of Value", fontsize=14)
+ax.tick_params(labelsize=14)
+# ax.plot(input_values,squares,linewidth=1)
+
+ax.axis([0,1100,0,1_100_000])
 plt.show()
