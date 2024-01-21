8ed2480e135e76c5f89c96c3d5b02f3761853182
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-21 06:24:47 +0100

    graph line thickness n others

--- project2_generating_data/mpl_squares.py
+++ project2_generating_data/mpl_squares.py
@@ -3,6 +3,10 @@ import matplotlib.pyplot as plt
 squares = [1, 4, 9, 16, 25]
 
 fig, ax = plt.subplots()
-ax.plot(squares)
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Value", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+# ax.tick_params(labelsize=14)
+ax.plot(squares,linewidth=1)
 
 plt.show()
