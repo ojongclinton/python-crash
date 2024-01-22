0e9beaa73530982f8268c423fbe9999bc22c9a5b
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-22 21:58:17 +0100

    while true to create new walks graphs

--- project2_generating_data/mpl_squares.py
+++ project2_generating_data/mpl_squares.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-input_values = range(1,1001)
+input_values = range(1,5)
 squares = [x**2 for x in input_values]
 
 plt.style.use('seaborn')
@@ -11,8 +11,8 @@ ax.scatter(input_values, squares,c=squares ,cmap=plt.cm.Blues, s=1)
 ax.set_title("Square Numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)         
 ax.set_ylabel("Square of Value", fontsize=14)
-ax.tick_params(labelsize=14)
-# ax.plot(input_values,squares,linewidth=1)
+ax.tick_params(labelsize=10)
+ax.plot(input_values,squares,linewidth=1)
 
-ax.axis([0,1100,0,1_100_000])
+# ax.axis([0,1100,0,1_100_000])
 plt.show()
--- project2_generating_data/rw_visuals.py
+++ project2_generating_data/rw_visuals.py
@@ -1,12 +1,16 @@
 import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
-rw = RandomWalk()
-rw.fill_walk()
+while True:
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break;
+    rw = RandomWalk()
+    rw.fill_walk()
 
-plt.style.use('classic')
-fig,ax = plt.subplots()
+    plt.style.use('classic')
+    fig,ax = plt.subplots()
 
-ax.scatter(rw.x_values,rw.y_values,s=15)
-ax.set_aspect('equal')
-plt.show()
+    ax.scatter(rw.x_values,rw.y_values,s=15)
+    ax.set_aspect('equal')
+    plt.show()
