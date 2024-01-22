d1ac8c4e4f5b2ff277cb4141fb19e90197aed25f
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-23 00:13:33 +0100

    Rolling two dice and plotting results

--- project2_generating_data/die_visual.py
+++ project2_generating_data/die_visual.py
@@ -1,18 +1,23 @@
 from die import Die
 import plotly.express as px
 
-die = Die()
+die_1 = Die()
+die_2 = Die()
+
 results = []
-titles = {'x':"Result",'y':'Frequency or result'}
+labels = {'x':"Result",'y':'Frequency or result'}
+title = "Results of Rolling Two D6 dice 1,000 Times"
 
 for roll_num in range(1000):
-    result = die.roll()
+    result = die_1.roll() + die_2.roll()
     results.append(result)
+
 frequencies = []
-poss_results = range(1, die.num_sides+1)
+max_result = die_1.num_sides + die_2.num_sides
+poss_results = range(1, max_result+1)
 for value in poss_results:
     frequencie = results.count(value)
     frequencies.append(frequencie)
 
-fig = px.bar(x=poss_results, y=frequencies, labels=titles)
+fig = px.bar(x=poss_results, y=frequencies, labels=labels, title=title)
 fig.show()
