ec90a210d52dcdcb0de24ca75bcca1a7fa9b8c4b
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-22 22:09:47 +0100

    Rmeoved y and x axis so graph is more visible

--- project2_generating_data/rw_visuals.py
+++ project2_generating_data/rw_visuals.py
@@ -2,15 +2,21 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    keep_running = input("Make another walk? (y/n): ")
-    if keep_running == 'n':
-        break;
     rw = RandomWalk()
     rw.fill_walk()
 
     plt.style.use('classic')
     fig,ax = plt.subplots()
-
-    ax.scatter(rw.x_values,rw.y_values,s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,s=15,edgecolors='none')
+    ax.scatter(0,0,c='green',edgecolors='none',s=100)
+    ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
+    # ax.scatter(rw.x_values,rw.y_values,s=15)
     ax.set_aspect('equal')
+    ax.set_title("Random Walk",fontsize=24)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
     plt.show()
+    keep_running = input("Make another walk? (y/n): ")
+    if keep_running == 'n':
+        break;
