d7580f27990990fb4493f1bf68b1636830eeb1f6
ojong <82442816+ojongclinton@users.noreply.github.com>
2024-01-22 22:35:17 +0100

    make visible start and end of each point

--- project2_generating_data/rw_visuals.py
+++ project2_generating_data/rw_visuals.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     plt.style.use('classic')
-    fig,ax = plt.subplots()
+    fig,ax = plt.subplots(figsize=(10,6),dpi=128)
     point_numbers = range(rw.num_points)
-    ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,s=15,edgecolors='none')
+    ax.scatter(rw.x_values,rw.y_values,c=point_numbers,cmap=plt.cm.Blues,s=1,edgecolors='none')
     ax.scatter(0,0,c='green',edgecolors='none',s=100)
     ax.scatter(rw.x_values[-1],rw.y_values[-1],c='red',edgecolors='none',s=100)
     # ax.scatter(rw.x_values,rw.y_values,s=15)
